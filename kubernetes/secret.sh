1700168732f5fee237b4e0a883f9009c29e71542
Max Prettyjohns <heshoots9999@gmail.com>
2017-10-05 09:02:44 +0100

    initial commit

---
+++ kubernetes/secret.sh
@@ -0,0 +1,21 @@
+echo -n "GIT_API: "
+echo -n $GIT_API | base64 -w0
+echo ""
+echo -n "CB_USER: "
+echo -n $CB_USER | base64 -w0
+echo ""
+echo -n "CB_PASS: "
+echo -n $CB_PASS | base64 -w0
+echo ""
+echo -n "JENKINS_USER: "
+echo -n $JENKINS_USER | base64 -w0
+echo ""
+echo -n "JENKINS_PASS: "
+echo -n $JENKINS_PASS | base64 -w0
+echo ""
+echo -n "TRELLO_KEY: "
+echo -n $TRELLO_KEY | base64 -w0
+echo ""
+echo -n "TRELLO_TOKEN: "
+echo -n $TRELLO_TOKEN | base64 -w0
+echo ""
